330a577d68ec05e23ea27a93220e0a6c64054dca
Sue Utah <sutony@yahoo.com>
2024-01-16 16:48:51 +0800

    Add files via upload

---
+++ PredictSound.py
@@ -0,0 +1,74 @@
+import os
+import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+DATASET_PATH = 'data/mini_speech_commands'
+
+data_dir = pathlib.Path(DATASET_PATH)
+sample_file = data_dir/'no/01bb6a2a_nohash_0.wav'
+commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[commands != 'README.md']
+print('Commands:', commands)
+def decode_audio(audio_binary):
+  # Decode WAV-encoded audio files to `float32` tensors, normalized
+  # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
+  audio, _ = tf.audio.decode_wav(contents=audio_binary)
+  # Since all the data is single channel (mono), drop the `channels`
+  # axis from the array.
+  return tf.squeeze(audio, axis=-1)
+def get_label(file_path):
+  parts = tf.strings.split(
+      input=file_path,
+      sep=os.path.sep)
+  # Note: You'll use indexing here instead of tuple unpacking to enable this
+  # to work in a TensorFlow graph.
+  return parts[-2]
+def get_waveform_and_label(file_path):
+  label = get_label(file_path)
+  audio_binary = tf.io.read_file(file_path)
+  waveform = decode_audio(audio_binary)
+  return waveform, label
+AUTOTUNE = tf.data.AUTOTUNE
+def preprocess_dataset(files):
+  files_ds = tf.data.Dataset.from_tensor_slices(files)
+  output_ds = files_ds.map(
+      map_func=get_waveform_and_label,
+      num_parallel_calls=AUTOTUNE)
+  output_ds = output_ds.map(
+      map_func=get_spectrogram_and_label_id,
+      num_parallel_calls=AUTOTUNE)
+  return output_ds
+def get_spectrogram(waveform):
+  # Zero-padding for an audio waveform with less than 16,000 samples.
+  input_len = 16000
+  waveform = waveform[:input_len]
+  zero_padding = tf.zeros(
+      [16000] - tf.shape(waveform),
+      dtype=tf.float32)
+  # Cast the waveform tensors' dtype to float32.
+  waveform = tf.cast(waveform, dtype=tf.float32)
+  # Concatenate the waveform with `zero_padding`, which ensures all audio
+  # clips are of the same length.
+  equal_length = tf.concat([waveform, zero_padding], 0)
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      equal_length, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+def get_spectrogram_and_label_id(audio, label):
+  spectrogram = get_spectrogram(audio)
+  label_id = tf.argmax(label == commands)
+  return spectrogram, label_id
+sample_ds = preprocess_dataset([str(sample_file)])
+model = tf.keras.models.load_model("sound_model")
+for spectrogram, label in sample_ds.batch(1):
+  prediction = model(spectrogram)
+  plt.bar(commands, tf.nn.softmax(prediction[0]))
+  plt.title(f'Predictions for "{commands[label[0]]}"')
+  plt.show()
